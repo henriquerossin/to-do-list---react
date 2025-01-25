22de45607cc6512f348f6901fd9b2a7f91e133cc
Henrique Rossin <pro.henriquerossin@gmail.com>
2025-01-25 18:52:18 -0300

    adicionando atributos aos componentes

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,25 +1,41 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ler a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Pagar fatura do curso',
+    descricao: 'Pagar mensalidade do curso de Programação',
+    prioridade: 'urgente',
+    status: 'concluída'
+  },
+  {
+    titulo: 'Ler meu livro',
+    descricao: 'Ler It a Coisa',
+    prioridade: 'Normal',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            status={t.status}
+            titulo={t.titulo}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
